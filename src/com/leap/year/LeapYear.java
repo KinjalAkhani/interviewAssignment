6ac7c38379ca96ff276a1412fae212ff66e77701
kinjal <kinjal.akhani@streebo.com>
2020-04-30 15:44:11 +0530

    Added initial Java Project structure with one package and class.

---
+++ src/com/leap/year/LeapYear.java
@@ -0,0 +1,23 @@
+package com.leap.year;
+
+/**
+ * @author Kinjal Akhani
+ *
+ */
+public class LeapYear {
+
+	/**
+	 * 
+	 */
+	public LeapYear() {
+		
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello Leap Year");
+	}
+
+}
